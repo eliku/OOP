3393a31c1d142d109c63748298c97ed531395a5a
GeekBrains <kubasovae@mail.ru>
2021-12-27 01:04:07 +0400

    task 1

---
+++ Lesson2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AccountBank Bank = new AccountBank();
+            Bank.number = 123;
+            Bank.balance = 5;
+            Bank.type = Type.Insured;
+            Console.WriteLine(Bank.Info());
+        }
+    }
+}
