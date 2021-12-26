d1cb25709cb93238cbd7ca3395c5953ee0ae66ca
GeekBrains <kubasovae@mail.ru>
2021-12-27 01:43:30 +0400

    task3

---
+++ Lesson2_Task3/AccountBank.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2_Task3
+{
+    enum Type
+    {
+        Budget,
+        Insured,
+        Frozen,
+        Contract,
+        Correspondent,
+        Savings
+    };
+
+    class AccountBank
+    {
+        private int _number;          //номер счета
+        private int _balance;         //баланс
+        private Type _type;           //тип
+
+        public AccountBank(int Balance)
+        {
+            _balance = Balance;
+            _number++;
+        }
+
+        public AccountBank(Type types)
+        {
+            _type = types;
+            _number++;
+        }
+
+        public AccountBank(int Balance, Type types)
+        {
+            _balance = Balance;
+            _number++;
+            _type = types;
+        }
+        public string Info()
+        {
+            string str = $"Номер счета {_number}, баланс {_balance}, тип счета {_type}";
+            return str;
+        }
+    }
+}
