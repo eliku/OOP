8590a29d72bce0b55f4a9473ce91e09de6f66db0
GeekBrains <kubasovae@mail.ru>
2021-12-27 01:28:59 +0400

    task2

---
+++ Lesson2_task2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lesson2_task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AccountBank Bank = new AccountBank();
+            Console.WriteLine(Bank.Info());
+            Bank.inputAccount( 4, Type.Budget);
+            Console.WriteLine(Bank.Info());
+            Bank.inputAccount(5, Type.Contract);
+            Console.WriteLine(Bank.Info());
+        }
+    }
+}
