34bb21f4682eea4851eecff6b5bf5ce5d35694d7
GeekBrains <kubasovae@mail.ru>
2021-12-29 23:15:57 +0400

    Task 2

--- Lesson3/Program.cs
+++ Lesson3/Program.cs
@@ -6,7 +6,28 @@ namespace Lesson3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //первый счет
+            AccountBank Bank1 = new AccountBank();
+
+            Bank1.balance = 300;
+            Bank1.type = Type.Savings;
+            Bank1.number = 123;
+
+            Console.WriteLine(Bank1.Info());
+
+            //второй счет
+            AccountBank Bank2 = new AccountBank();
+
+            Bank2.balance = 460;
+            Bank2.type = Type.Frozen;
+            Bank2.number = 124;
+
+            Console.WriteLine(Bank2.Info());
+
+            Bank1.TransferMoney(Bank2, 23);
+            Console.WriteLine(Bank1.Info());
+            Console.WriteLine(Bank2.Info());
+
         }
     }
 }
